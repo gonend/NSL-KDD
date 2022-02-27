2e891c06c5ba9d0ad1968aa142dddc7757956adb
noaakl <62835962+noaakl@users.noreply.github.com>
2022-02-27 20:23:19 +0200

    grid search

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import precision_recall_curve, f1_score, accuracy_score, confusion_matrix, ConfusionMatrixDisplay, \
     recall_score, precision_score
 from rotation_forest import RotationForestClassifier
-# from xgboost import XGBClassifier
+from xgboost import XGBClassifier
 
 from sklearn.model_selection import GridSearchCV
 
@@ -63,9 +63,18 @@ def run_models(tuning=False, strat=False, shap_flag=True, train=None, test=None)
     RoF = RotationForestClassifier()  # max_depth=3 , criterion='entropy', min_samples_split=20)
     LR = LogisticRegression()
     Adb = AdaBoostClassifier()
+    xgb = XGBClassifier()
     # RoF = RotationForestClassifier(class_weight=class_weights, min_samples_split=5) #, criterion='entropy')
     # RF = RandomForestClassifier(class_weight=class_weights, min_samples_split=5) #, criterion='entropy')
-    models = [DT, RF, RoF, Adb]  # , xgb]
+
+    # DT = DecisionTreeClassifier(criterion='entropy', max_depth=3, min_samples_split=3)
+    # RF = RandomForestClassifier(criterion='entropy', max_depth=3, min_samples_split=3,
+    #                        n_estimators=20)
+    # RoF = RotationForestClassifier(criterion='entropy', max_depth=15, min_samples_split=5,
+    #                          n_estimators=20)
+    # Adb = AdaBoostClassifier(learning_rate=1, n_estimators=20)
+
+    models = [DT, RF, RoF, Adb, xgb]  # , xgb]
 
     if not tuning:
         print("-" * 40 + "Runing models without improvments" + "-" * 40)
@@ -85,23 +94,53 @@ def run_models(tuning=False, strat=False, shap_flag=True, train=None, test=None)
         # RoF = RotationForestClassifier(n_estimators=50, max_depth=5, criterion='gini',
         #                                min_samples_split=20)  # max_depth=3 , criterion='entropy', min_samples_split=20)
         # Adb = AdaBoostClassifier(n_estimators=100, learning_rate=1)
-        DT = DecisionTreeClassifier()  #
-        RF = RandomForestClassifier()  #
-        RoF = RotationForestClassifier()  # max_depth=3 , criterion='entropy', min_samples_split=20)
-        Adb = AdaBoostClassifier()
 
-        models_params = dict()
-        models_params[DT] = {'max_depth': [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, None], 'min_samples_split': [3, 5, 10, 20],
-                             'criterion': ['entropy', 'gini']}
-        models_params[RF] = {'n_estimators': [20, 50, 100, 120], 'max_depth': [3, 5, 8, 9, 10, 15, None],
-                             'criterion': ['entropy', 'gini'], 'min_samples_split': [3, 5, 10, 20]}
-        models_params[RoF] = {'n_estimators': [20, 50, 100, 120], 'max_depth': [3, 5, 8, 9, 10, 15, None],
-                              'criterion': ['entropy', 'gini'], 'min_samples_split': [3, 5, 10, 20]}
-        models_params[Adb] = {'n_estimators': [20, 50, 100, 120], 'learning_rate': [1, 2, 3, 4 ,5]}
 
-        models = [DT, RF, RoF, Adb]  # , xgb]
+
+        # # grid search
+        # DT = DecisionTreeClassifier()  #
+        # RF = RandomForestClassifier()  #
+        # RoF = RotationForestClassifier()  # max_depth=3 , criterion='entropy', min_samples_split=20)
+        # Adb = AdaBoostClassifier()
+        # xgb = XGBClassifier()
+        #
+        #
+        # models_params = dict()
+        # models_params[DT.__str__()] = {'max_depth': [3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 'min_samples_split': [2, 3, 5, 10, 20],
+        #                      'criterion': ['entropy', 'gini']}
+        # models_params[RF.__str__()] = {'n_estimators': [10, 20, 50, 100, 120], 'max_depth': [3, 5, 8, 9, 10, 15, None],
+        #                      'criterion': ['entropy', 'gini'], 'min_samples_split': [2, 3, 5, 10, 20]}
+        # models_params[RoF.__str__()] = {'n_estimators': [10, 20, 50, 100, 120], 'max_depth': [3, 5, 8, 9, 10, 15, None],
+        #                       'criterion': ['entropy', 'gini'], 'min_samples_split': [2, 3, 5, 10, 20]}
+        # models_params[Adb.__str__()] = {'n_estimators': [20, 50, 100, 120], 'learning_rate': [1.0, 2.0, 3.0, 4.0, 5.0]}
+        # models_params[xgb.__str__()] = {'n_estimators': [20, 50, 100, 120], 'use_label_encoder': [True, False]}
+
+        # DT = DecisionTreeClassifier(criterion='gini', max_depth=3)
+        # RF = RandomForestClassifier(criterion='entropy', max_depth=3, n_estimators=20)
+        # RoF = RotationForestClassifier(criterion='entropy', max_depth=3)
+        # Adb = AdaBoostClassifier(n_estimators=20)
+        # xgb = XGBClassifier()
+
+        DT = DecisionTreeClassifier(criterion='entropy', max_depth=10)
+        RF = RandomForestClassifier(criterion='entropy', max_depth=5, n_estimators=10)
+        RoF = RotationForestClassifier(criterion='entropy', max_depth=3, n_estimators=50)
+        Adb = AdaBoostClassifier(n_estimators=20)
+        xgb = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+                      colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
+                      gamma=0, gpu_id=-1, importance_type=None,
+                      interaction_constraints='', learning_rate=0.300000012,
+                      max_delta_step=0, max_depth=6, min_child_weight=1,
+                      monotone_constraints='()', n_estimators=20, n_jobs=16,
+                      num_parallel_tree=1, predictor='auto', random_state=0,
+                      reg_alpha=0, reg_lambda=1, scale_pos_weight=1, subsample=1,
+                      tree_method='exact', validate_parameters=1, verbosity=None)
+
+
+
+        models = [DT, RF, RoF, Adb, xgb]
+        # models = [xgb]  # , xgb]
         models_accuracy = {}
-        skf = StratifiedKFold(n_splits=7)
+        skf = StratifiedKFold(n_splits=4)
         for model in models:
             if strat:
                 models_accuracy[model] = []
@@ -114,30 +153,22 @@ def run_models(tuning=False, strat=False, shap_flag=True, train=None, test=None)
                     models_accuracy[model].append(score)
                 # print('-' * 40 + model.__class__.__name__ + '-' * 40)
 
-                param_grid = models_params[model]
-                base_estimator = model
-                X, y = make_classification(n_samples=1000, random_state=0)
-                sh = HalvingGridSearchCV(base_estimator, param_grid, cv=5,
-                                         factor=2,
-                                         max_resources=30).fit(X, y)
-                sh.best_estimator_
-                print(sh.best_estimator_)
-
                 evaluate(model, X_train, y_train, X_test, y_test, fit=False)
                 print(f"validation acc: {np.array(models_accuracy[model]).mean()}")  # .mean()
             else:
+                pass
+                # param_grid = models_params[model.__str__()]
+                # base_estimator = model
+                # X, y = X_train, y_train
+                # sh = HalvingGridSearchCV(base_estimator, param_grid, cv=5,
+                #                          factor=2,
+                #                          max_resources=30).fit(X, y)
+                # sh.best_estimator_
+                # print(sh.best_estimator_)
 
-                param_grid = models_params[model]
-                base_estimator = model
-                X, y = make_classification(n_samples=1000, random_state=0)
-                sh = HalvingGridSearchCV(base_estimator, param_grid, cv=5,
-                                         factor=2, resource='n_estimators',
-                                         max_resources=30).fit(X, y)
-                sh.best_estimator_
-                print(sh.best_estimator_)
 
-                evaluate(model, X_train, y_train, X_test, y_test, fit=True)
-                # evaluate(model, X_train, y_train, X_train, y_train, fit=True)
+
+                # evaluate(model, X_train, y_train, X_test, y_test, fit=True)
 
             # y_pred = model.predict(X_test)
             # print(f"test acc: {accuracy_score(y_pred,y_test)}")
@@ -191,7 +222,7 @@ if __name__ == '__main__':
     # run_models(tuning=True, strat=False, shap_flag=False, train=train, test=test)
 
     print("-" * 40 + "Using StratifiedKFold" + "-" * 40)
-    run_models(tuning=True, strat=True, shap_flag=False, train=train, test=test)
+    run_models(tuning=True, strat=True, shap_flag=True, train=train, test=test)
 
 #    for train_inx, val_inx in skf.split(X_train, y_train):
 #     X_train1, X_val = X_train.iloc[train_inx], X_train.iloc[val_inx]
